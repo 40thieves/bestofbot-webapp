f8786ad5cf933bde456cbaa70c7749ed75f6c64c
Alasdair Smith <alasdairsmith100@gmail.com>
2014-08-21 22:26:30 +0100

    Env specific config checked for existence before requiring

--- app/config/config.js
+++ app/config/config.js
@@ -8,9 +8,12 @@ process.env.NODE_ENV = ~fs.readdirSync('./app/config/env').map(function(file) {
 	return file.slice(0, -3);
 }).indexOf(process.env.NODE_ENV) ? process.env.NODE_ENV : 'development';
 
+// Private env-specific config in .env.ENV_NAME.js 
+var envConfigPath = path.normalize(__dirname + '/../../.env.' + process.env.NODE_ENV + '.js');
+
 // Extend base config with env config
 module.exports = _.extend(
 	require('./env/all'),
 	require('./env/' + process.env.NODE_ENV || {}),
-	require(path.normalize('../../.env.' + process.env.NODE_ENV + '.js'))
+	fs.existsSync(envConfigPath) ? require(envConfigPath) : null
 );
