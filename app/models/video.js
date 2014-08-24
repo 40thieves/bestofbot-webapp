c5daf7d0efa63fd290754d4c902a5972c61dd71e
Alasdair Smith <alasdairsmith100@gmail.com>
2014-08-24 18:11:31 +0100

    Clean up video schema

--- app/models/video.js
+++ app/models/video.js
@@ -10,10 +10,10 @@ var VideoSchema = new Schema({
 		type: String,
 		required: true
 	},
-	// timestamp: {
-	// 	type: String,
-	// 	required: true
-	// },
+	timestamp: {
+		type: String,
+		required: true
+	},
 	description: {
 		type: String,
 		trim: true
@@ -31,36 +31,5 @@ var VideoSchema = new Schema({
 		default: Date.now
 	}
 });
-
-/**
- * Validations
- */
-var validatePresenceOf = function(value) {
-	return value && value.length;
-};
-
-// VideoSchema.path('title').validate(function(title) {
-// 	return (validatePresenceOf(title) && typeof title === 'string');
-// }, 'Title cannot be blank');
-
-// VideoSchema.path('url').validate(function(url) {
-// 	return (validatePresenceOf(url) && typeof url === 'string');
-// }, 'URL cannot be blank');
-
-/*
- * Statics
- */
-// VideoSchema.statics = {
-// 	*
-// 	 * Find by id and populate user obj
-// 	 * @param  {ObjectId}   id Link id
-// 	 * @param  {Function}   cb Success/failure callback
-	 
-// 	load: function(id, cb) {
-// 		this.findOne({
-// 			_id: id
-// 		}).populate('user', 'name username').exec(cb);
-// 	}
-// };
 
 mongoose.model('Video', VideoSchema);
