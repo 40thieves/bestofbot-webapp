4029540920db2f81972af4f3ae3a44f9e6f97fdf
Alasdair Smith <alasdairsmith100@gmail.com>
2014-09-13 13:22:40 +0100

    Index controller pagination

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,17 +4,31 @@ var mongoose = require('mongoose')
 ;
 
 exports.render = function(req, res, next) {
-	Video.paginate({}, req.query.page, req.query.limit, function(err, pageCount, videos, itemCount) {
+	var currentPage = req.query.page || 1;
+	var limit = req.query.limit || 5;
+
+	Video.paginate({}, req.query.page, limit, function(err, pageCount, videos, itemCount) {
 		if (err) {
 			console.log('err', err.message);
 			next(err);
 		}
 
+		var pages = [];
+		for (i = 0; i < pageCount; i++) {
+			var pageNum = i + 1;
+
+			pages.push({
+				number: pageNum,
+				url: '?page=' + pageNum
+			});
+		}
+
 		res.render('index', {
 			videos: videos,
+			pages: pages,
+			currentPage: currentPage,
 			pageCount: pageCount,
 			itemCount: itemCount
 		});
 	});
-
 };
