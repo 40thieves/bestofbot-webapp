be8609594aa6623782e215a6e0835f4dd36860d8
Alasdair Smith <alasdairsmith100@gmail.com>
2014-10-08 22:10:02 +0100

    Cleans up timestamp code, now only moments within first 5 mins will jump back 5 mins to make timestamp. After this will only jump back 1 min

--- app/services/TimestampCalcService.js
+++ app/services/TimestampCalcService.js
@@ -11,25 +11,52 @@ var TimestampService = function(options) {
 		extend(this.options, options);
 };
 
-TimestampService.prototype.options = {
-	bestOfStart: {
-		time: '5',
-		order: 'minutes'
-	}
-};
+TimestampService.prototype.options = {};
 
 TimestampService.prototype.getFiveMinsAgo = function() {
-	return new moment().subtract(this.options.bestOfStart.time, this.options.bestOfStart.order);
+	return new moment().subtract('5', 'minutes');
 };
 
-TimestampService.prototype.calcTimeDiff = function(videoStartStr) {
+TimestampService.prototype.getOneMinAgo = function() {
+	return new moment().subtract('1', 'minutes');
+};
+
+TimestampService.prototype.isAtVideoStart = function(videoStart) {
 	var bestOfStart = this.getFiveMinsAgo();
-	var videoStart = new moment(videoStartStr, moment.ISO_8601);
+	var diff = bestOfStart.diff(videoStart, 'seconds');
+
+	return diff < 5;
+};
+
+TimestampService.prototype.getShortDiff = function(videoStart) {
+	var bestOfStart = this.getOneMinAgo();
 
 	var diff = bestOfStart.diff(videoStart, 'seconds');
 
-	// Prevent 'negative' timestamps, and just return beginning of video
-	if (diff < 0) return '0';
+	if (diff < 0) return '0'; // Prevent 'negative' timestamps, and just return beginning of video
+	return diff;
+};
+
+TimestampService.prototype.getLongDiff = function(videoStart) {
+	var bestOfStart = this.getFiveMinsAgo();
+
+	var diff = bestOfStart.diff(videoStart, 'seconds');
+
+	if (diff < 0) return '0'; // Prevent 'negative' timestamps, and just return beginning of video
+	return diff;
+};
+
+TimestampService.prototype.calcTimeDiff = function(videoStartStr) {
+	var videoStart = new moment(videoStartStr, moment.ISO_8601)
+	,	diff
+	;
+
+	// If at video start, start video further back
+	// This handles YT API issue where it takes a long time to 
+	if (this.isAtVideoStart(videoStart))
+		diff = this.getLongDiff(videoStart);
+	else
+		diff = this.getShortDiff(videoStart);
 
 	var minsAgo = Math.floor(diff / 60);
 	var secsAgo = diff % 60;
